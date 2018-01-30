48d1366036142dd80f39eb1293dadb74e2f786ad
tomasz-piotrowicz <pantomash@gmail.com>
2018-01-30 20:20:03 +0100

    1.1

--- script.js
+++ script.js
@@ -112,7 +112,7 @@ $(document).ready(function() {
 
     $.ajax({
       url: requestUrl + '/' + $.param({
-        id: taskId
+        taskId: taskId
       }),
       method: 'DELETE',
       success: function() {
